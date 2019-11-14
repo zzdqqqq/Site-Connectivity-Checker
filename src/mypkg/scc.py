6dff7d5a11e3ebb84254b01cd6089d59afff9f6e
zidong <2018zhangzd@gmail.com>
2019-11-13 21:44:48 -0500

    rearrange simple file to a module

--- src/mypkg/scc.py
+++ src/mypkg/scc.py
@@ -3,44 +3,61 @@ import argparse
 import re
 import time
 
-parser = argparse.ArgumentParser(prog= "python3 main.py 'www.example.com'", description="Site-Connectivity-Checker")
-parser.add_argument("-hn", dest="host_name", action="store", help="python3 scc.py ['host name']")
-parser.add_argument("-debug", dest="debug", action="store_true", help="debug mode")
-parser.add_argument("-save", dest="save_file", action="store_true", help="save HTTP header file")
-parser.add_argument("-buffer", dest="buffer_size", action="store", help="set buffer size to store http header and body")
+class Factory():
+    def create_parser(args):
+        parser = argparse.ArgumentParser(prog= "python3 scc.py 'www.example.com'", description="Site-Connectivity-Checker")
+        parser.add_argument("-hn", dest="host_name", action="store", help="python3 scc.py ['host name']")
+        parser.add_argument("-debug", dest="debug", action="store_true", help="debug mode")
+        parser.add_argument("-save", dest="save_file", action="store_true", help="save HTTP header file")
+        parser.add_argument("-buffer", dest="buffer_size", action="store", help="set buffer size to store http header and body")
 
-args = parser.parse_args()
-print("Connecting to: ", args.host_name)
-port = 80
+        return parser
 
-if not args.buffer_size:
-    args.buffer_size = 20
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((args.host_name, 80))
-        s.sendall(b"GET / HTTP/1.0\r\n\r\n")
-        data = s.recv(int(args.buffer_size))
+class SiteChecker():
+    def __init__(self, args):
+        # Get data
+        # self.args = parser.parse_args()
+        self.args = args
+    
+        print("Connecting to: ", self.args.host_name)
+        self.port = 80
 
-    data = data.decode('utf-8')
-    if args.debug:
-        print(data, "\n")
+        if not self.args.buffer_size:
+            self.args.buffer_size = 20
 
-    if args.save_file:
-        with open("http_data.txt", "w+") as f:
-            f.write(data)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.args.host_name, 80))
+            s.sendall(b"GET / HTTP/1.0\r\n\r\n")
+            data = s.recv(int(self.args.buffer_size))
 
-    match_obj = re.match(r"^(HTTP/1...)([0-9][0-9][0-9]{1})", data)
-    if match_obj:
-        if match_obj.group(2) == '200':
-            print("200 OK")
-            break
-        elif match_obj.group(2) == '500':
-            print("500 Internal Server Error")
-        else:
-            print("NOT OK")
-    time.sleep(2)
+        self.data = data.decode('utf-8')
 
-# TODO
+    def scc(self):
+        match_obj = re.match(r"^(HTTP/1...)([0-9][0-9][0-9]{1})", self.data)
+        if match_obj:
+            if match_obj.group(2) == '200':
+                # print("200 OK")
+                return "200 OK"
+            elif match_obj.group(2) == '500':
+                return "500 Internal Server Error"
+            else:
+                return "NOT OK"
+
+    def scc_debug(self):
+            if self.args.save_file:
+                with open("http_data.txt", "w+") as f:
+                    f.write(self.data)
+
+            if self.args.debug:
+                return self.data
+
 if __name__ == "__main__":
     # Do some test
-    pass
+
+    parser = Factory().create_parser()
+    args = parser.parse_args()
+    checker = SiteChecker(args)
+    print(checker.scc())
+    debug = checker.scc_debug()
+    if debug:
+        print(debug)
