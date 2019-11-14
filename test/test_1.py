dbb23ae576313869f3dd2bcb7e83be5dba77be6a
zidong <2018zhangzd@gmail.com>
2019-11-13 21:45:22 -0500

    add true test

--- test/test_1.py
+++ test/test_1.py
@@ -1,4 +1,27 @@
 import pytest
+import unittest
+import argparse
+from mypkg.scc import SiteChecker, Factory
 
-def test_case1():
-    assert(1 == 1)
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = Factory().create_parser()
+
+    def test_case1(self):
+        # args.host_name = "www.zidong.us"
+        args = self.parser.parse_args(["-hn", "www.zidong.us"])
+
+        res = "200 OK"
+        assert(res == SiteChecker(args).scc())
+
+    def test_case2(self):
+        args = self.parser.parse_args(["-hn", "www.google.com"])
+
+        res = "200 OK"
+        assert(res == SiteChecker(args).scc())
+    
+    def test_case3(self):
+        args = self.parser.parse_args(["-hn", "www.youtube.com"])
+
+        res = "200 OK"
+        assert(res == SiteChecker(args).scc())
