a27912f5074bb052c07f5a418688eadb3b715ce0
zidong <2018zhangzd@gmail.com>
2019-11-19 14:59:48 -0500

    Add more error detection

--- src/mypkg/scc.py
+++ src/mypkg/scc.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import argparse
 import re
@@ -5,11 +6,20 @@ import time
 
 class Factory():
     def create_parser(args):
-        parser = argparse.ArgumentParser(prog= "python3 scc.py 'www.example.com'", description="Site-Connectivity-Checker")
-        parser.add_argument("-hn", dest="host_name", action="store", help="python3 scc.py ['host name']")
-        parser.add_argument("-debug", dest="debug", action="store_true", help="debug mode")
-        parser.add_argument("-save", dest="save_file", action="store_true", help="save HTTP header file")
-        parser.add_argument("-buffer", dest="buffer_size", action="store", help="set buffer size to store http header and body")
+        parser = argparse.ArgumentParser(prog= "python3 scc.py 'www.example.com'",
+                                         description="Site-Connectivity-Checker")
+        parser.add_argument("-hn", dest="host_name",
+                                   action="store", 
+                                   help="python3 scc.py ['host name']")
+        parser.add_argument("-debug", dest="debug", 
+                                      action="store_true", 
+                                      help="debug mode")
+        parser.add_argument("-save", dest="save_file", 
+                                     action="store_true", 
+                                     help="save HTTP header file")
+        parser.add_argument("-buffer", dest="buffer_size", 
+                                       action="store", 
+                                       help="set buffer size to store http header and body")
 
         return parser
 
@@ -18,6 +28,7 @@ class SiteChecker():
         # Get data
         # self.args = parser.parse_args()
         self.args = args
+        self.any_error = False
     
         print("Connecting to: ", self.args.host_name)
         self.port = 80
@@ -26,14 +37,29 @@ class SiteChecker():
             self.args.buffer_size = 20
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.args.host_name, 80))
+            try:
+                # s.connect(("www.www", self.port))
+                s.connect((self.args.host_name, 80))
+            except socket.gaierror:
+                print("[ERROR]socket.gaierror: Please enter a correct URL! \n \
+                    if you confirm the URL is correct, just ignore the error log.\n \
+                    Wait until the site response.")
+                self.any_error = True
+                s.connect(('', self.port))
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                self.any_error = True
+                s.connect(('', self.port))
+                raise
             s.sendall(b"GET / HTTP/1.0\r\n\r\n")
             data = s.recv(int(self.args.buffer_size))
 
         self.data = data.decode('utf-8')
 
     def scc(self):
-        match_obj = re.match(r"^(HTTP/1...)([0-9][0-9][0-9]{1})", self.data)
+        if self.any_error:
+            return "Check error log."
+        match_obj = re.match(r"^(HTTP\/1...)([0-9][0-9][0-9]{1})", self.data)
         if match_obj:
             if match_obj.group(2) == '200':
                 # print("200 OK")
@@ -53,11 +79,16 @@ class SiteChecker():
 
 if __name__ == "__main__":
     # Do some test
-
-    parser = Factory().create_parser()
-    args = parser.parse_args()
-    checker = SiteChecker(args)
-    print(checker.scc())
-    debug = checker.scc_debug()
-    if debug:
-        print(debug)
+    print(sys.argv)
+    while True:
+        parser = Factory().create_parser()
+        args = parser.parse_args()
+        checker = SiteChecker(args)
+        res = checker.scc()
+        print(res)
+        debug = checker.scc_debug()
+        if debug:
+            print(debug)
+        if res == "200 OK":
+            break
+        time.sleep(2)
